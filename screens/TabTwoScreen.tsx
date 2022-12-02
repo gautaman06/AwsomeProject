e87095bad790b237c5b281846f72d7a95891ac42
Gautaman E <gautaman1701@gmail.com>
2022-12-02 08:10:55 +0530

    fix(root): react

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -10,8 +10,8 @@ export default function TabTwoScreen() {
       <Text style={styles.title}>Tab Two</Text>
       <View
         style={styles.separator}
-        lightColor="#eee"
-        darkColo="rgba(255,255,255,0.1)"
+        lightColo="#eee"
+        darkColor="rgba(255,255,255,0.1)"
       />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
     </View>
