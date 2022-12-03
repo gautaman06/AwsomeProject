d7a0c2c6f1e8f961f59c9151c32e339fd8ca4f7f
Gautaman E <gautaman1701@gmail.com>
2022-12-03 23:43:15 +0530

    fix(lint): resolved

--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -11,8 +11,7 @@ export default function NotFoundScreen({
     <View style={styles.container}>
       <Text style={styles.title}>This screen doesn't exist.</Text>
       <TouchableOpacity
-        onP
-        ress={() => navigation.replace("Root")}
+        onPress={() => navigation.replace("Root")}
         style={styles.link}
       >
         <Text style={styles.linkText}>Go to home screen!</Text>
