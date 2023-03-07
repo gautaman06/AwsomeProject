49be8d9b2513d15a204a3a2272fc0841e6009950
Gautaman E - Lumel <gautamane@lumel.com>
2023-03-07 09:30:53 +0530

    feat(colors): theme colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,30 @@
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
+export const white = '#fff';
+export const black = '#000';
+
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: black,
+    background: white,
     tint: tintColorLight,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
+    borderBottomColor: black,
+    borderTopColor: white,
+    borderLeftColor: white,
+    borderRightColor: white,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: white,
+    background: black,
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
+    borderBottomColor: white,
+    borderTopColor: black,
+    borderLeftColor: black,
+    borderRightColor: black,
   },
 };
