b7b16ad8de9c4c5079947955fc3f97d85f4c397f
Gautaman E - Lumel <gautamane@lumel.com>
2023-05-14 12:30:21 +0530

    feat(firebase): operators

---
+++ firebase/fireBaseConstants.ts
@@ -0,0 +1,12 @@
+export const QUERY_OPERATORS = {
+  EQUAL: '==',
+  IN: 'in',
+  NOT_IN: 'not-in',
+  LESS_THAN: '<',
+  LESS_THAN_OR_EQAUL: '<=',
+  GREATER_THAN: '>',
+  GREATER_THAN_OR_EQAUL: '>=',
+  NOT_EQUAL: '!=',
+  ARRAY_CONTAINS: 'array-contains',
+  ARRAY_CONTAINS_ANY: 'array-contains-any',
+};
