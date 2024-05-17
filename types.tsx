effbff3f7550c7ed4111667f0433a2cf6180b869
Gautaman E - Lumel <gautamane@lumel.com>
2024-05-17 22:10:22 +0530

    feat(splieasy): groups updates

--- types.tsx
+++ types.tsx
@@ -28,7 +28,7 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = Nati
 >;
 
 export type RootTabParamList = {
-  TabOne: undefined;
+  Groups: undefined;
   TabTwo: undefined;
 };
 
